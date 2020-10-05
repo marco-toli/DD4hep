6136c6b57ff2bfaa1ffa248bbb18d7aa04d85970
Sarah <sarah.eno@cern.ch>
2020-10-05 11:21:04 -0400

    update

--- DDDetectors/compact/SCEPCALplot.C
+++ DDDetectors/compact/SCEPCALplot.C
@@ -1,44 +1,72 @@
 #include "TROOT.h"
 #include "TFile.h"
 #include "TTree.h"
+#include "TBranch.h"
 #include "TBrowser.h"
 #include "TH2.h"
 #include "TRandom.h"
+#include "DD4hep/Printout.h"
+#include "DD4hep/Objects.h"
+#include "DD4hep/Factories.h"
+#include "DDG4/Geant4Particle.h"
+#include "DDG4/Geant4Data.h"
+
+#include <vector>
 
 const float sampling_fraction=100.;
 
 void SCEPCALplot() {
 
-  const char* inputfilename="testSCEPCAL.root";
+
+  // read in libraries that define the classes
+  Long_t result;
+  char text[1024];
+  const char* dd4hep = gSystem->Getenv("DD4hepINSTALL");
+  snprintf(text,sizeof(text)," -I%s/include -D__DD4HEP_DDEVE_EXCLUSIVE__ -Wno-shadow -g -O0",dd4hep);
+  gSystem->AddIncludePath(text);
+  TString fname = "libDDG4IO";
+  const char* io_lib = gSystem->FindDynamicLibrary(fname,kTRUE);
+  result = gSystem->Load("libDDG4IO");
+  result = gSystem->Load("libDDEvePlugins");
+
+
+  // open data and output file for histograms
+  const char* inputfilename="/data/users/eno/dd4hep/DD4hep/DDDetectors/compact/testSCEPCAL.root";
   const char* outputfilename="testSCEPCAL_hist.root";
 
+
+  // define histograms
   TH1F *hgenPsize = new TH1F("hgenPsize","number of generator particles",600,0.,40000);
+  TH1F *hgenPdgID = new TH1F("hgenpdgID","pdgID of generator particles",600,-1000,1000);
 
 
+  // get Tree
   TFile *f = new TFile(inputfilename);
-  TTree *t1 = (TTree*)f->Get("EVENT");
+  TTreeReader myReader("EVENT",f);
 
-  vector<float> *inputMomentum = new vector<float>;
-  int genP_size;  
+  // get objects
+  TTreeReaderValue<std::vector<dd4hep::sim::Geant4Particle*>> myMCParticles(myReader, "MCParticles");
+  TTreeReaderValue<std::vector<dd4hep::sim::Geant4Calorimeter::Hit*>> myEcalBarrelHits(myReader, "EcalBarrelHits");
 
+  unsigned int evtCounter = 0;
+  while (myReader.Next() && evtCounter<10){
+    cout << "Event " << evtCounter++ << endl;
+    std::cout<<" number of gen particles "<< myMCParticles->size() <<std::endl;
+    dd4hep::sim::Geant4Particle* itmp = myMCParticles->at(0);
+    //    std::cout<<"  first particle id pt is "<<(*itmp)->pdgID()<<std::endl;
 
-
-  t1->SetBranchAddress("MCParticles/@size",&genP_size);
-
-
-  Int_t nentries = (Int_t)t1->GetEntries();
-  for(Int_t i=0;i<nentries; i++) {
-    t1->GetEntry(i);
-    std::cout<<" number of gen particles "<<genP_size<<std::endl;
-    hgenPsize->Fill(genP_size);
-		    
+    for(int ipart=0; ipart < myMCParticles->size(); ipart++){
+        hgenPdgID->Fill((myMCParticles->at(ipart))->pdgID);
+        if(ipart<5) cout << "particle " << (*(myMCParticles->at(ipart))).pdgID << endl;
+    }
+    hgenPsize->Fill( myMCParticles->size());
   }
-
+  
   f->Close();
 
   TFile * out = new TFile(outputfilename,"RECREATE");
   hgenPsize->Write();
-
+  hgenPdgID->Write();
   out->Close();
 
 }
